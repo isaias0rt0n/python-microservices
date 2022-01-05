bccbb1e524bafa078142b7357907cb4d559d6073
isaias0rt0n <isaiasorton@gmail.com>
2022-01-04 22:48:10 -0300

    finishing python apps with microservices:docker, RabbtMQ(Producer & Consumer)

---
+++ consumer.py
@@ -0,0 +1,24 @@
+import pika
+
+params = pika.URLParameters('your_rabbitmq_url')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+
+def callback(ch, method, properties, body):
+    print('Received in admin')
+    print(body)
+
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
+
---
+++ products/producer.py
@@ -0,0 +1,14 @@
+import pika, json
+
+params = pika.URLParameters('your_rabbitmq_url')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
+
+    # exchange = ''  -> default: procura exatamente uma chave com nome da routing_key
